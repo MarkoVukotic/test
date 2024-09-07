0721533698e112d80e29156c5e6038f88d81702e
Marko Vukotic <44193845+MarkoVukotic@users.noreply.github.com>
2024-09-07 11:37:50 +0200

    Update test-cdn.js

--- test-cdn.js
+++ test-cdn.js
@@ -4262,3 +4262,94 @@ const by_accommodation_type_chart = new Chart(by_accommodation_type_ctx, by_acco
 
 
 
+const country_benchmarking_chart_2_ctx = document.querySelector('[data-element="country_benchmarking_chart_2"]');
+// if (!country_benchmarking_chart_2_ctx) return;
+
+const labels_country_benchmarking_chart_2 = ['Portugal', 'Spain', 'Austria', 'Denmark', 'Sweden', 'Poland', 'France', 'Finland', 'Italy', 'Germany', 'Czechia', 'Netherlands', 'Ireland', 'Belgium', 'Iceland', 'United Kingdom'];
+const data_country_benchmarking_chart_2 = {
+  labels: labels_country_benchmarking_chart_2,
+  datasets: [
+    {
+      label: '  MHI-5   ',
+      data: ['64.84', '61.04', '60.57', '60.24', '59.79', '59.79', '58.66', '57.82', '57.68', '57.26', '56.88', '56.59', '55.43', '55.21' , '54.32' ,'51.83'],
+      borderColor: '#80C7BD',
+      backgroundColor: transparentize('#80C7BD', 0.8),
+      order: 1
+    },
+    {
+      label: '  Traditional Net Promoter Score    ',
+    data: ['41.40', '20.93', '3.60', '9.61', '25.65', '26.41', '-15.55', '24.75', '-1.07', '-19.94', '-32.14', '6.07', '-30.35', '-58.42' , '-2.04' ,'-5.00'],
+      borderColor: '#FF5200',
+      backgroundColor: transparentize('#FF5200', 1),
+      type: 'line',
+      order: 0
+    }
+    ,
+    {
+      label: '  MH Impact Net Promoter Score    ',
+     data: ['10.15', '1.62', '-10.59', '-28.12', '-16.10', '-0.47', '-26.34', '-12.65', '-23.38', '-19.81', '-27.85', '-26.72', '-38.14', '-48.31' , '-35.83' ,'-34.49'],
+      borderColor: '#f5e846',
+      backgroundColor: transparentize('#f5e846', 1),
+      type: 'line',
+      order: 0
+    }
+  ]
+};
+
+const config_country_benchmarking_chart_2 = {
+  type: 'bar',
+  data: data_country_benchmarking_chart_2,
+  options: {
+    barPercentage: 0.5,
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+        labels: {
+          usePointStyle: true,
+          boxWidth: 6
+        }
+      },
+      title: {
+        display: true,
+        text: 'country_benchmarking_chart_2 TITLE'
+      }
+    },
+    scales: {
+      y: {
+        type: 'linear',
+        display: true,
+        position: 'left',
+        min: 50,
+        max: 65,
+        title: {
+          display: true,
+          text: 'MHI-5'
+        },
+      },
+      y1: {
+        type: 'linear',
+        display: true,
+        position: 'right',  
+        min: -35.0,
+        max: 15.0,
+        title: {
+          display: true,
+          text: 'MH Impact Net Promoter Score'
+        },
+        ticks: {stepSize: 10},
+        grid: {
+          drawOnChartArea: false, // only want the grid lines for one axis to show up
+        }
+      }
+    },
+  },
+};
+
+const chart_country_benchmarking_chart_2 = new Chart(country_benchmarking_chart_2_ctx, config_country_benchmarking_chart_2);
+
+
+
+
+
+
